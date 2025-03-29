b479d69cc582b7078d9ed14e5b85072a7b3ed9ae
venomous-maker <morganokumu8+1@gmail.com>
2025-03-29 14:55:00 +0300

    Updated logger functions docstrings

--- src/libs/classes/Logger.h
+++ src/libs/classes/Logger.h
@@ -6,8 +6,26 @@
 #include "../headers/ProcessManager.h"
 using namespace  processManager;
 // Logger: Captures and logs output, resource usage, and execution time
+/**
+ * @file Logger.h
+ *
+ * @brief Logs output, resource usage, and execution time of executed commands
+ *
+ * The logger class is responsible for capturing and logging output, resource
+ * usage, and execution time of executed commands. It logs to a file and
+ * provides methods to log messages, resource usage, and execution time.
+ *
+ */
 class Logger : public ILogger {
 public:
+    /**
+     * @brief Construct a new Logger object
+     *
+     * @param logFile The name of the log file to write to.
+     *
+     * Opens the log file in append mode and logs any errors encountered to
+     * standard error.
+     */
     explicit Logger(const std::string& logFile) : logStream(logFile, std::ios::out | std::ios::app) {
         if (!logStream) {
             std::cerr << "Error: Could not open log file: " << logFile << std::endl;
@@ -15,21 +33,46 @@ public:
     }
 
 
+    /**
+     * @brief Logs a message to the log file
+     *
+     * @param message The message to log
+     */
     void log(const std::string& message) override {
         logStream << message << std::endl;
         logStream.flush();
     }
 
+    /**
+     * @brief Logs resource usage information
+     *
+     * Logs CPU time, memory usage, I/O read, and I/O write
+     *
+     * @param usage Resource usage information
+     */
     void logResourceUsage(const IResourceMonitor::ResourceUsage& usage) override {
         logStream << "CPU Time: " << usage.cpuTime << " sec, "
                   << "Memory Usage: " << usage.memoryUsage << " KB, "
                   << "I/O Read: " << usage.ioRead << " bytes, "
                   << "I/O Write: " << usage.ioWrite << " bytes" << std::endl;
     }
-
+    /**
+     * @brief Logs execution time of a command
+     *
+     * @param time Execution time in seconds
+     */
     void logExecutionTime(double time) override {
         logStream << "Execution Time: " << time << " seconds" << std::endl;
     }
+
+    /**
+     * @brief Sets the name of the log file to write to
+     *
+     * If the log file is currently open, it is closed before the new log file is opened.
+     * If the new log file cannot be opened, an error message is logged to standard error.
+     *
+     * @param filename The name of the new log file
+     */
     void setFilename(std::string filename) override {
         logStream.close();
         logStream.open(filename, std::ios::out | std::ios::app);
@@ -39,6 +82,11 @@ public:
         this->filename = std::move(filename);
     }
 
+    /**
+     * @brief Destructor for Logger class
+     *
+     * If the log file is currently open, it is closed.
+     */
     ~Logger() override {
         if (logStream.is_open()) {
             logStream.close();
@@ -47,6 +95,12 @@ public:
 
 
 private:
+    /**
+     * @brief Stream to write log messages to a file
+     *
+     * This stream is used to output log messages, including general messages,
+     * resource usage, and execution time, to a specified log file.
+     */
     std::ofstream logStream;
 };
 
